b38a7b80725306c743b8b45b718b458e7f4a61cd
KOSA <KOSA@DESKTOP-4U4G4G2>
2024-08-19 11:04:22 +0900

    일단 stt추가됨

--- chatbot/src/main/java/com/example/chatbot/controller/ApiController.java
+++ chatbot/src/main/java/com/example/chatbot/controller/ApiController.java
@@ -4,10 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -67,16 +65,5 @@ public class ApiController {
 			map.put("result", "failed");
 		
 		return new ResponseEntity<>(map, HttpStatus.OK);
-    }
-    
-    //stt api
-    @Value("${STT_API_KEY}")
-    private String apiKey;
-
-    @GetMapping("/api-key")
-    public Map<String, String> getApiKey() {
-        Map<String, String> response = new HashMap<>();
-        response.put("apiKey", apiKey);
-        return response;
     }
 }
