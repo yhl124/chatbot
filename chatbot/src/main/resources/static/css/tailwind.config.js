7c4343d53dbc366e43efba1bdcf5fadabe6cb6c6
kosa <kosa@host.docker.internal>
2024-08-20 16:50:37 +0900

    색상변경, 에러페이지 추가

---
+++ chatbot/src/main/resources/static/css/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      textColor: {
+        'custom-color': '#7167D3', // 원하는 16진수 색상을 여기에 추가
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
